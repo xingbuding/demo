36b6d7380bdb2d1717e0c562f72e4f6d8fe7bb0d
LuYangXu <2832736770@qq.com>
2023-04-10 20:47:24 +0800

    xie

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sound.midi.Soundbank;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        System.out.println("ayhvbskjabnefd");
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
